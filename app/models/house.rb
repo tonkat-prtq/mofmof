3cef7102b8e4423afc094c14739f18fbaf1a93c9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-01-30 21:37:34 +0900

    Add:house,station両モデルにバリデーション追加

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,7 @@
 class House < ApplicationRecord
+  has_many :stations
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
 end
