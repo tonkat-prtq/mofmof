a0820aefa222cc5795bf4e6d619678822fc7c854
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-02-03 14:54:45 +0900

    Add:House、Stationの両方の情報を一つのフォームで登録出来るように

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,12 +1,24 @@
 class HousesController < ApplicationController
   def index
+    @houses = House.all
   end
 
   def new
     @house = House.new
+    @house.stations.build
   end
 
   def create
+    @house = House.new(house_params)
+    if params[:back]
+      render :new
+    else
+      if @house.save
+        # redirect_to houses_path
+      else
+        render :new
+      end
+    end
   end
 
   def edit
@@ -20,4 +32,11 @@ class HousesController < ApplicationController
 
   def show
   end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:name, :price, :address, :age, :comment, stations_attributes: %i(id line name take_minutes))
+  end
+
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,6 @@
 class House < ApplicationRecord
   has_many :stations
+  accepts_nested_attributes_for :stations
   validates :name, presence: true
   validates :price, presence: true
   validates :address, presence: true
